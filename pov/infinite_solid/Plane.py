a46200f099917f5318be2bb84e4c0d557c190378
Chocokiko <chokokiko@gmx.de>
2013-11-05 19:11:06 +0100

    Plane syntax checks

--- pov/infinite_solid/Plane.py
+++ pov/infinite_solid/Plane.py
@@ -29,6 +29,46 @@ class Plane(BlockObject):
 
         super(Plane, self).__init__('plane', [normal, distance], opts, kwargs)
 
+        #@Todo: Generalize handling of boolean keywords
         if 'hollow' in self.kwargs and self.kwargs['hollow'] is True:
             del self.kwargs['hollow']
             self.opts.append('hollow')
+
+    def _check_arguments(self):
+        '''
+            Argument Syntax checks
+        '''
+        valid_args = ['Vector', ('float', 'int')]
+
+        self._validate_args(valid_args)
+
+        # param syntax checks
+        if not len(self.args[0].v) == 3:
+            raise SdlSyntaxException('Normal vector has more or less than 3 dimensions')
+
+    def _check_opts(self):
+        '''
+            Option Syntax checks
+
+            @Todo: get rid of Object Modifier superclass?
+        '''
+        valid_opts = ['ObjectModifier']
+
+        self._validate_opts(valid_opts)
+
+    def _check_kwargs(self):
+        '''
+            Keyword Argument Syntax checks
+        '''
+
+        valid_kw = {
+            # Object modifier kw
+            'no_shadow': 'bool',
+            'no_image': 'bool',
+            'no_reflection': 'bool',
+            'inverse': 'bool',
+            'double_illuminate': 'bool',
+            'hollow': 'bool'
+        }
+
+        self._validate_kwargs(valid_kw)
