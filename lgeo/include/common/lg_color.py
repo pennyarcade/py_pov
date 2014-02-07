4cb48637833d6d70b595ef6820767feaf97c6a7b
steyner <steyner@gmx.de>
2014-02-07 23:26:57 +0100

    - some more work on lg_color

--- lgeo/include/common/lg_color.py
+++ lgeo/include/common/lg_color.py
@@ -95,168 +95,168 @@ lg_black = Texture(
 
 # 1
 lg_blue = Texture(
- pigment { rgb <0/255, 51/255, 178/255> ),
- Finish( lg_solid_finish ),
-),
+     Pigment(Color(rgb=Vector(0/255, 51/255, 178/255)),
+     Finish( lg_solid_finish )
+)
 
 # 2
 lg_green = Texture(
- pigment { rgb <0/255, 140/255, 20/255> ),
+ Pigment(Color(rgb=Vector(0/255, 140/255, 20/255)),
  Finish( lg_solid_finish ),
 ),
 
 # 3
 lg_teal = Texture(
- pigment { rgb <0/255, 153/255, 159/255> ),
+ Pigment(Color(rgb=Vector(0/255, 153/255, 159/255)),
  Finish( lg_solid_finish ),
 ),
 lg_cyan = lg_teal
 
 # 4
 lg_red = Texture(
- pigment { rgb <196/255, 0/255, 38/255> ),
+ Pigment(Color(rgb=Vector(196/255, 0/255, 38/255)),
  Finish( lg_solid_finish ),
 ),
 
 # 5
 lg_dark_pink = Texture(
- pigment { rgb <223/255, 102/255, 149/255> ),
+ Pigment(Color(rgb=Vector(223/255, 102/255, 149/255)),
  Finish( lg_solid_finish ),
 ),
 
 # 6
 lg_brown = Texture(
- pigment { rgb <92/255, 32/255, 0/255> ),
+ Pigment(Color(rgb=Vector(92/255, 32/255, 0/255)),
  Finish( lg_solid_finish ),
 ),
 
 # 7
 lg_grey = Texture(
- pigment { rgb <193/255, 194/255, 193/255> ),
+ Pigment(Color(rgb=Vector(193/255, 194/255, 193/255)),
  Finish( lg_solid_finish ),
 ),
 
 # 8
 lg_dark_grey = Texture(
- pigment { rgb <99/255, 89/255, 82/255> ),
+ Pigment(Color(rgb=Vector(99/255, 89/255, 82/255)),
  Finish( lg_solid_finish ),
 ),
 
 # 9
 lg_light_blue = Texture(
- pigment { rgb <107/255, 172/255, 220/255> ),
+ Pigment(Color(rgb=Vector(107/255, 172/255, 220/255)),
  Finish( lg_solid_finish ),
 ),
 
 # 10
 lg_bright_green = Texture(
- pigment { rgb <97/255, 238/255, 104/255> ),
+ Pigment(Color(rgb=Vector(97/255, 238/255, 104/255)),
  Finish( lg_solid_finish ),
 ),
 
 # 11
 lg_turquoise = Texture(
- pigment { rgb <51/255, 166/255, 167/255> ),
+ Pigment(Color(rgb=Vector(51/255, 166/255, 167/255)),
  Finish( lg_solid_finish ),
 ),
 lg_blue_green = lg_turquoise
 
 # 12
 lg_salmon = Texture(
- pigment { rgb <255/255, 133/255, 122/255> ),
+ Pigment(Color(rgb=Vector(255/255, 133/255, 122/255)),
  Finish( lg_solid_finish ),
 ),
 lg_light_red = lg_salmon
 
 # 13
 lg_pink = Texture(
- pigment { rgb <249/255, 163/255, 198/255> ),
+ Pigment(Color(rgb=Vector(249/255, 163/255, 198/255)),
  Finish( lg_solid_finish ),
 ),
 lg_rose = lg_pink
 
 # 14
 lg_yellow = Texture(
- pigment { rgb <255/255, 220/255, 0/255> ),
+ Pigment(Color(rgb=Vector(255/255, 220/255, 0/255)),
  Finish( lg_solid_finish ),
 ),
 
 # 15
 lg_white = Texture(
- pigment { rgb <255/255, 255/255, 255/255> ),
+ Pigment(Color(rgb=Vector(255/255, 255/255, 255/255)),
  Finish( lg_solid_finish ),
 ),
 
 # 17
 lg_light_green = Texture(
- pigment { rgb <186/255, 255/255, 206/255> ),
+ Pigment(Color(rgb=Vector(186/255, 255/255, 206/255)),
  Finish( lg_solid_finish ),
 ),
 
 # 18
 lg_light_yellow = Texture(
- pigment { rgb <253/255, 232/255, 150/255> ),
+ Pigment(Color(rgb=Vector(253/255, 232/255, 150/255)),
  Finish( lg_solid_finish ),
 ),
 
 # 19
 lg_tan = Texture(
- pigment { rgb <232/255, 207/255, 161/255> ),
+ Pigment(Color(rgb=Vector(232/255, 207/255, 161/255)),
  Finish( lg_solid_finish ),
 ),
 
 # 20
 lg_light_violet = Texture(
- pigment { rgb <215/255, 196/255, 230/255> ),
+ Pigment(Color(rgb=Vector(215/255, 196/255, 230/255)),
  Finish( lg_solid_finish ),
 ),
 
 # 22
 lg_purple = Texture(
- pigment { rgb <129/255, 0/255, 124/255> ),
+ Pigment(Color(rgb=Vector(129/255, 0/255, 124/255)),
  Finish( lg_solid_finish ),
 ),
 lg_violet = lg_purple
 
 # 23
 lg_violet_blue = Texture(
- pigment { rgb <71/255, 50/255, 176/255> ),
+ Pigment(Color(rgb=Vector(71/255, 50/255, 176/255)),
  Finish( lg_solid_finish ),
 ),
 
 # 25
 lg_orange = Texture(
- pigment { rgb <249/255, 96/255, 0/255> ),
+ Pigment(Color(rgb=Vector(249/255, 96/255, 0/255)),
  Finish( lg_solid_finish ),
 ),
 
 # 26
 lg_magenta = Texture(
- pigment { rgb <232/255, 27/255, 109/255> ),
+ Pigment(Color(rgb=Vector(232/255, 27/255, 109/255)),
  Finish( lg_solid_finish ),
 ),
 
 # 27
 lg_lime = Texture(
- pigment { rgb <231/255, 240/255, 0/255> ),
+ Pigment(Color(rgb=Vector(231/255, 240/255, 0/255)),
  Finish( lg_solid_finish ),
 ),
 
 # 27
 lg_dark_tan = Texture(
- pigment { rgb <187/255, 141/255, 75/255> ),
+ Pigment(Color(rgb=Vector(187/255, 141/255, 75/255)),
  Finish( lg_solid_finish ),
 ),
 
 # 29
 lg_light_purple = Texture(
- pigment { rgb <205/255, 173/255, 200/255> ),
+ Pigment(Color(rgb=Vector(205/255, 173/255, 200/255)),
  Finish( lg_solid_finish ),
 ),
 
 # 33
 lg_clear_blue = Texture(
- pigment { rgb <0/255, 32/255, 160/255>
+ Pigment(Color(rgb=Vector(0/255, 32/255, 160/255>
   #if (lg_quality > 1)
    filter 0.9
   #end
@@ -266,7 +266,7 @@ lg_clear_blue = Texture(
 
 # 34
 lg_clear_green = Texture(
- pigment { rgb <6/255, 98/255, 50/255>
+ Pigment(Color(rgb=Vector(6/255, 98/255, 50/255>
   #if (lg_quality > 1)
    filter 0.9
   #end
@@ -276,7 +276,7 @@ lg_clear_green = Texture(
 
 # 36
 lg_clear_red = Texture(
- pigment { rgb <196/255, 0/255, 38/255>
+ Pigment(Color(rgb=Vector(196/255, 0/255, 38/255>
   #if (lg_quality > 1)
    filter 0.9
   #end
@@ -286,7 +286,7 @@ lg_clear_red = Texture(
 
 # 37
 lg_clear_violet = Texture(
- pigment { rgb <100/255, 0/255, 97/255>
+ Pigment(Color(rgb=Vector(100/255, 0/255, 97/255>
   #if (lg_quality > 1)
    filter 0.9
   #end
@@ -296,7 +296,7 @@ lg_clear_violet = Texture(
 
 # 40
 lg_clear_brown = Texture(
- pigment { rgb <99/255, 89/255, 82/255>
+ Pigment(Color(rgb=Vector(99/255, 89/255, 82/255>
   #if (lg_quality > 1)
    filter 0.9
   #end
@@ -306,7 +306,7 @@ lg_clear_brown = Texture(
 
 # 41
 lg_clear_cyan = Texture(
- pigment { rgb <174/255, 239/255, 237/255>
+ Pigment(Color(rgb=Vector(174/255, 239/255, 237/255>
   #if (lg_quality > 1)
    filter 0.9
   #end
@@ -316,7 +316,7 @@ lg_clear_cyan = Texture(
 
 # 42
 lg_clear_neon_yellow = Texture(
- pigment { rgb <192/255, 255/255, 0/255>
+ Pigment(Color(rgb=Vector(192/255, 255/255, 0/255>
   #if (lg_quality > 1)
    filter 0.85
   #end
@@ -326,7 +326,7 @@ lg_clear_neon_yellow = Texture(
 
 # 45
 lg_clear_pink = Texture(
- pigment { rgb <223/255, 102/255, 149/255>
+ Pigment(Color(rgb=Vector(223/255, 102/255, 149/255>
   #if (lg_quality > 1)
    filter 0.9
   #end
@@ -336,7 +336,7 @@ lg_clear_pink = Texture(
 
 # 46
 lg_clear_yellow = Texture(
- pigment { rgb <202/255, 176/255, 0/255>
+ Pigment(Color(rgb=Vector(202/255, 176/255, 0/255>
   #if (lg_quality > 1)
    filter 0.9
   #end
@@ -346,7 +346,7 @@ lg_clear_yellow = Texture(
 
 # 47
 lg_clear = Texture(
- pigment { rgb <255/255, 255/255, 255/255>
+ Pigment(Color(rgb=Vector(255/255, 255/255, 255/255>
   #if (lg_quality > 1)
    filter 0.9
   #end
@@ -356,7 +356,7 @@ lg_clear = Texture(
 
 # 57
 lg_clear_neon_orange = Texture(
- pigment { rgb <249/255, 96/255, 0/255>
+ Pigment(Color(rgb=Vector(249/255, 96/255, 0/255>
   #if (lg_quality > 1)
    filter 0.8
   #end
@@ -366,204 +366,204 @@ lg_clear_neon_orange = Texture(
 
 # 69
 lg_bright_purple = Texture(
- pigment { rgb <205/255, 98/255, 152/255> ),
+ Pigment(Color(rgb=Vector(205/255, 98/255, 152/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 70
 lg_reddish_brown = Texture(
- pigment { rgb <105/255, 64/255, 39/255> ),
+ Pigment(Color(rgb=Vector(105/255, 64/255, 39/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 71
 lg_bluish_grey = Texture(
- pigment { rgb <163/255, 162/255, 164/255> ),
+ Pigment(Color(rgb=Vector(163/255, 162/255, 164/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 72
 lg_dark_bluish_grey = Texture(
- pigment { rgb <99/255, 95/255, 97/255> ),
+ Pigment(Color(rgb=Vector(99/255, 95/255, 97/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 73
 lg_medium_blue = Texture(
- pigment { rgb <110/255, 153/255, 201/255> ),
+ Pigment(Color(rgb=Vector(110/255, 153/255, 201/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 74
 lg_medium_green = Texture(
- pigment { rgb <161/255, 196/255, 139/255> ),
+ Pigment(Color(rgb=Vector(161/255, 196/255, 139/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 77
 lg_paradisa_pink = Texture(
- pigment { rgb <254/255, 204/255, 204/255> ),
+ Pigment(Color(rgb=Vector(254/255, 204/255, 204/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 78
 lg_light_flesh = Texture(
- pigment { rgb <250/255, 215/255, 195/255> ),
+ Pigment(Color(rgb=Vector(250/255, 215/255, 195/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 85
 lg_medium_violet = Texture(
- pigment { rgb <52/255, 43/255, 117/255> ),
+ Pigment(Color(rgb=Vector(52/255, 43/255, 117/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 86
 lg_dark_flesh = Texture(
- pigment { rgb <124/255, 92/255, 69/255> ),
+ Pigment(Color(rgb=Vector(124/255, 92/255, 69/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 89
 lg_royal_blue = Texture(
- pigment { rgb <155/255, 178/255, 239/255> ),
+ Pigment(Color(rgb=Vector(155/255, 178/255, 239/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 92
 lg_flesh = Texture(
- pigment { rgb <204/255, 142/255, 104/255> ),
+ Pigment(Color(rgb=Vector(204/255, 142/255, 104/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 134
 lg_pearl_copper = Texture(
- pigment { color rgb <147/255, 135/255, 103/255> ),
+ Pigment( colorColor(rgb=Vector(147/255, 135/255, 103/255> ),
  Finish( lg_pearl_finish ),
 ),
 
 # 135
 lg_pearl_grey = Texture(
- pigment { color rgb <171/255, 173/255, 172/255> ),
+ Pigment( colorColor(rgb=Vector(171/255, 173/255, 172/255> ),
  Finish( lg_pearl_finish ),
 ),
 
 # 137
 lg_pearl_blue = Texture(
- pigment { color rgb <106/255, 122/255, 150/255> ),
+ Pigment( colorColor(rgb=Vector(106/255, 122/255, 150/255> ),
  Finish( lg_pearl_finish ),
 ),
 
 # 142
 lg_pearl_gold = Texture(
- pigment { color rgb <215/255, 169/255, 75/255> ),
+ Pigment( colorColor(rgb=Vector(215/255, 169/255, 75/255> ),
  Finish( lg_pearl_finish ),
 ),
 
 # 151
 lg_very_light_bluish_grey = Texture(
- pigment { rgb <229/255, 228/255, 222/255> ),
+ Pigment(Color(rgb=Vector(229/255, 228/255, 222/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 272
 lg_dark_blue = Texture(
- pigment { rgb <0/255, 29/255, 104/255> ),
+ Pigment(Color(rgb=Vector(0/255, 29/255, 104/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 288
 lg_dark_green = Texture(
- pigment { rgb <39/255, 70/255, 44/255> ),
+ Pigment(Color(rgb=Vector(39/255, 70/255, 44/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 320
 lg_dark_red = Texture(
- pigment { rgb <120/255, 0/255, 28/255> ),
+ Pigment(Color(rgb=Vector(120/255, 0/255, 28/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 313
 lg_maersk_blue = Texture(
- pigment { rgb <53/255, 162/255, 189/255> ),
+ Pigment(Color(rgb=Vector(53/255, 162/255, 189/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 334
 lg_gold_chrome = Texture(
- pigment { rgb <233/255, 110/255, 19/255> ),
+ Pigment(Color(rgb=Vector(233/255, 110/255, 19/255> ),
  Finish( lg_chrome_finish ),
 ),
 
 # 373
 lg_sand_purple = Texture(
- pigment { rgb <132/255, 94/255, 132/255> ),
+ Pigment(Color(rgb=Vector(132/255, 94/255, 132/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 335
 lg_sand_red = Texture(
- pigment { rgb <191/255, 135/255, 130/255> ),
+ Pigment(Color(rgb=Vector(191/255, 135/255, 130/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 366
 lg_earth_orange = Texture(
- pigment { rgb <209/255, 131/255, 4/255> ),
+ Pigment(Color(rgb=Vector(209/255, 131/255, 4/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 373
 lg_sand_purple = Texture(
- pigment { rgb <132/255, 94/255, 132/255> ),
+ Pigment(Color(rgb=Vector(132/255, 94/255, 132/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 378
 lg_sand_green = Texture(
- pigment { rgb <160/255, 188/255, 172/255> ),
+ Pigment(Color(rgb=Vector(160/255, 188/255, 172/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 379
 lg_sand_blue = Texture(
- pigment { rgb <106/255, 122/255, 150/255> ),
+ Pigment(Color(rgb=Vector(106/255, 122/255, 150/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 383
 lg_chrome = Texture(
- pigment { rgb <232/255, 232/255, 232/255> ),
+ Pigment(Color(rgb=Vector(232/255, 232/255, 232/255> ),
  Finish( lg_chrome_finish ),
 ),
 
 # 462
 lg_light_orange = Texture(
- pigment { rgb <254/255, 159/255, 6/255> ),
+ Pigment(Color(rgb=Vector(254/255, 159/255, 6/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 484
 lg_dark_orange = Texture(
- pigment { rgb <179/255, 62/255, 0/255> ),
+ Pigment(Color(rgb=Vector(179/255, 62/255, 0/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 503
 lg_very_light_grey = Texture(
- pigment { rgb <230/255, 227/255, 219/255> ),
+ Pigment(Color(rgb=Vector(230/255, 227/255, 219/255> ),
  Finish( lg_solid_finish ),
 ),
 
 # 999
 lg_undefined = Texture(
- pigment { rgb <0.5, 0.7, 0.9> ),
+ Pigment(Color(rgb=Vector(0.5, 0.7, 0.9> ),
 ),
 
 # 998 (no code known)
 lg_medium_orange = Texture(
- pigment { rgb <224/255, 129/255, 6/255> ),
+ Pigment(Color(rgb=Vector(224/255, 129/255, 6/255> ),
  Finish( lg_solid_finish ),
 ),
 
