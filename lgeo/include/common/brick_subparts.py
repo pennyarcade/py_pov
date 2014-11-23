df91151b7055275d6e8c0e942495962e86bd2788
chocokiko <chocokiko4@gmx.de>
2014-11-23 20:06:40 +0100

    fix linter issues

--- lgeo/include/common/brick_subparts.py
+++ lgeo/include/common/brick_subparts.py
@@ -16,7 +16,7 @@ placing the parts by hand. In LGEO, for historic reasons Y axis is the depth
 axis, and Z is the up axis, where also Z has switched sign to LDRAW Y axis.
 Sorry for that, but my understanding of coordinate to this time was a flat
 standard XY coordinate system with a third Z axis coming out of the plane.
-Scaling is to real measurements, where 1 POV-Ray unit is 10mm. So 0.8 is 20 
+Scaling is to real measurements, where 1 POV-Ray unit is 10mm. So 0.8 is 20
 LDU (width of a LEGO brick) and 0.96 is 24 LDU (height of a LEGO brick).
 ***************************************************************************'''
 
@@ -31,28 +31,31 @@ from pov.object_modifier.Translate import Translate
 
 from pov.other.Object import Object
 
-from lgeo.include.common.lg_defs import *
+from lgeo.include.common.lg_defs import LG_WALL_WIDTH, LG_BRICK_WIDTH
+from lgeo.include.common.lg_defs import LG_PLATE_INNER_HEIGHT, LG_CORNER_SPACE
+from lgeo.include.common.lg_defs import LG_BRICK_INNER_HEIGHT
 
 '''**************************************************************************
 LGEO Standard Brick common subparts
 **************************************************************************'''
 
-def get_knob_inner_space(LENGTH=1, WIDTH=1):
+
+def get_knob_inner_space(length=1, width=1):
     result = Union(
         Box(
             Vector(LG_WALL_WIDTH, LG_WALL_WIDTH, -LG_CORNER_SPACE),
             Vector(
-                LENGTH*LG_BRICK_WIDTH-LG_WALL_WIDTH,
-                WIDTH*LG_BRICK_WIDTH-LG_WALL_WIDTH,
+                length*LG_BRICK_WIDTH-LG_WALL_WIDTH,
+                width*LG_BRICK_WIDTH-LG_WALL_WIDTH,
                 LG_PLATE_INNER_HEIGHT
             )
         )
     )
 
     KS_X = 0
-    while (KS_X < LENGTH):
+    while (KS_X < length):
         KS_Y = 0
-        while (KS_Y < WIDTH):
+        while (KS_Y < width):
             result.append(
                 Object(
                     lg_knob_inner_space,
@@ -71,13 +74,13 @@ def get_knob_inner_space(LENGTH=1, WIDTH=1):
     return result
 
 
-def get_knob_objects(LENGTH=1, WIDTH=1, HEIGHT=LG_BRICK_HEIGHT):
+def get_knob_objects(length=1, width=1, height=LG_BRICK_HEIGHT):
     result = Union()
 
     KNOB_X = 0
-    while (KNOB_X < LENGTH):
+    while (KNOB_X < length):
         KNOB_Y = 0
-        while (KNOB_Y < WIDTH):
+        while (KNOB_Y < width):
             result.append(
                 Object(
                     lg_knob(),
@@ -86,7 +89,7 @@ def get_knob_objects(LENGTH=1, WIDTH=1, HEIGHT=LG_BRICK_HEIGHT):
                         Vector(
                             (0.5+KNOB_X)*LG_BRICK_WIDTH,
                             (0.5+KNOB_Y)*LG_BRICK_WIDTH,
-                            HEIGHT
+                            height
                         )
                     ),
                 )
@@ -97,11 +100,11 @@ def get_knob_objects(LENGTH=1, WIDTH=1, HEIGHT=LG_BRICK_HEIGHT):
     return result
 
 
-def get_brick_coloumn(LENGTH=1):
+def get_brick_coloumn(length=1):
     result = Union()
 
     COL_X = 1
-    while (COL_X < LENGTH):
+    while (COL_X < length):
         result.append(
             Object(
                 lg_brick_column,
